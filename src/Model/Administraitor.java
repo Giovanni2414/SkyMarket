0d1c68c444baa1b57e73617231fc6b4350d70b0e
Giovanni <Giovanni2414g@gmail.com>
2020-12-04 00:43:00 -0500

    Part of contracts

--- src/Model/Administraitor.java
+++ src/Model/Administraitor.java
@@ -7,21 +7,28 @@ import java.util.LinkedList;
 public class Administraitor extends User{
 
 	/**
-	 * 
+	 * Serial version UID
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * ArrayList to save and show all users in the app
+	 */
 	private ArrayList<User> users;
+	
+	/**
+	 * ArrayList to save and show all articles in the app
+	 */
 	private ArrayList<Article> articles;
 	
-	/*
-	name: Administraitor
-	Its the constructor of Administraitor
-	@param: name
-	@param: lastName
-	@param: identification
-	@param: password
-	@param: username
-	@param: picture
+	/**
+	* Its the constructor of Administraitor
+	* @param: name
+	* @param: lastName
+	* @param: identification
+	* @param: password
+	* @param: username
+	* @param: picture
 	**/
 	public Administraitor(String name, String lastName, String identification,String email, String password, String username, String picture, LocalDate birthday,LinkedList<User> users) {
 		super(name,lastName,identification,email,password,username,picture,birthday);
@@ -29,18 +36,42 @@ public class Administraitor extends User{
 		//duda con cambio de LinkedList a ArrayList
 	}
 
+	/**
+	 * Getter of ArrayList users
+	 * <br><b>Pre:<b> ArrayList users must be initializated
+	 * <br><b>Post:<b>
+	 * @return The ArrayList with the users
+	 */
 	public ArrayList<User> getUsers() {
 		return users;
 	}
 
+	/**
+	 * Setter of users ArrayList
+	 * <br><b>Pre:<b> 
+	 * <br><b>Post:<b> A new list of users has been setted
+	 * @param users 
+	 */
 	public void setUsers(ArrayList<User> users) {
 		this.users = users;
 	}
 
+	/**
+	 * Getter of articles ArrayList
+	 * <br><b>Pre:<b> ArrayList articles must be initializated
+	 * <br><b>Post:<b>
+	 * @return Return the ArrayList with articles
+	 */
 	public ArrayList<Article> getArticles() {
 		return articles;
 	}
 
+	/**
+	 * Setter of Articles ArrayList
+	 * <br><b>Pre:<b>
+	 * <br><b>Post:<b> A new ArrayList with articles has been setted
+	 * @param articles The new ArrayList with articles
+	 */
 	public void setArticles(ArrayList<Article> articles) {
 		this.articles = articles;
 	}
